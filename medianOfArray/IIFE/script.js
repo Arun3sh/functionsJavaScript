95134eb2e5b64481c76672b43ee353316ef61bf7
Aruneshwaran <65992799+Arun3sh@users.noreply.github.com>
2021-06-23 08:47:17 +0530

    IIFE Function

---
+++ medianOfArray/IIFE/script.js
@@ -0,0 +1,17 @@
+(function(){
+  let a = [21, 43, 65, 87, 9];
+  let b = [45, 12, 34, 15, 75];
+
+  let n1 = a.length;
+  let n2 = b.length;
+  let n = n1+n2;
+  if(n1 == n2){
+    let c = [...a, ...b];
+    let sum = 0;
+    c.forEach(ele =>{
+      sum += ele;
+    });
+    console.log(sum/n);
+  }
+
+})();
