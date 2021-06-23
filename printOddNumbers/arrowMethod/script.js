74c3e9c517415c51eb61d5a07f1509aa52c95def
Aruneshwaran <65992799+Arun3sh@users.noreply.github.com>
2021-06-23 08:09:47 +0530

    arrow Method function

---
+++ printOddNumbers/arrowMethod/script.js
@@ -0,0 +1,22 @@
+res = () => {
+  var m = document.getElementById("from").value;
+  var n = document.getElementById("to").value;
+  
+  var a = [];
+  var b = +m;
+  if(+m > +n){
+    console.log("Enter correct Value");
+    return;
+  }
+  for(let i = 0; i<(n-m); i++){
+    a[i] = b;
+    b += 1;
+  }
+  for(let j = 0; j<(n-m); j++){
+    if(+a[j]%2 != 0){
+      console.log(a[j]);
+      document.getElementById("answer").innerHTML += a[j] + " \n";
+    }
+  }
+  return;
+}
